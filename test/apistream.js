731765fe166325ff9eace5f36ffdcb15ceed6798
alec <alec.hulce@gmail.com>
2015-03-25 00:08:21 -0700

    (ง°ل͜°)ง

--- test/apistream.js
+++ test/apistream.js
@@ -11,10 +11,10 @@ var apistream = require('../lib/apistream')
 
 apistream('https://stream.twitter.com/1.1/statuses/filter.json',{
 	data: {
-		//locations: '23.830106,-170.534369,70.416383,-52.497261' // us/ca
+		locations: '23.830106,-170.534369,70.416383,-52.497261' // us/ca
 		//locations: '-122.75,36.8,-121.75,37.8' // sf
 		//follow: 'jewelmint'
-		track: 'pizza'
+		//track: 'pizza'
 	}
 })
 .on('softerror',function(err){
